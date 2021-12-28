8b1453eb48d46014dfe4418c21fb839e899b5dac
Vashesh Jogani <vashesh2001@gmail.com>
2021-12-28 09:55:18 +0530

    Update hidden.py

--- Coursera/hidden.py
+++ Coursera/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "Mbn7LecKAIvWznCp0tSWlgtQc",
-            "consumer_secret": "G19hLMIVZHnZbw3NjQVTlzq1B8lSrDYFCWHIi2JYvTNNuAeCzz",
-            "token_key": "1013816891746504706-CmOuCIhMIyeo16zLoNvJlEyxUoXTIk",
-            "token_secret": "tF8b8rCyG3f4dDUMMJyB0HXMRCJRY7fm5aHgOIG30NCBH"}
+    return {"consumer_key": "___",
+            "consumer_secret": "___",
+            "token_key": "____",
+            "token_secret": "___"}
